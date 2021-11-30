596ec48660d367f7d0cb428b6795c4e7beaafa9e
Damien <93924263+DamienTudor@users.noreply.github.com>
2021-11-30 08:40:24 +0100

    Add files via upload

--- MonsterHunter_NewWorld/gestionfichier.pas
+++ MonsterHunter_NewWorld/gestionfichier.pas
@@ -26,7 +26,7 @@ begin
              MkDir('C:\MHNewWorld');
              miseAjourLVL(personnage1,1);
              miseAjourExp(personnage1,0);
-             miseAjourOr(personnage1,0);
+             miseAjourOr(personnage1,500);
              miseAjourPersonnage(personnage1,0);
              creationInventaireItem(0,65); // En rajouter si on ajoute des slots
         end;
